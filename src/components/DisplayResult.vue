c04eb2904a6b6707adcd4e42c28ecfeb672df00b
João Victor Davi <j.victordavid2@gmail.com>
2022-05-01 14:18:40 -0300

    feat: create Diplay Component

---
+++ src/components/DisplayResult.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="display">
+    <span>0,00000000000000000</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayResult',
+  components: {
+  }
+}
+</script>
+
+<style>
+.display {
+  max-height: 100%;
+  max-width: calc(4 * 25%);
+  grid-column: span 4;
+  background-color: #0009;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  padding: 0.5em;
+  font-size: 2em;
+}
+
+.display span {
+  display: inline;
+  max-height: 2em;
+  overflow-y: auto;
+  overflow-x: auto;
+}
+</style>
