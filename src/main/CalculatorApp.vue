c969690472e77669550394ae8fc9e33d1f927b2a
João Victor Davi <j.victordavid2@gmail.com>
2022-05-01 14:18:57 -0300

    feat: create Calculator component

---
+++ src/main/CalculatorApp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="calculator">
+    <display-result></display-result>
+    <button-base :label="'AC'" :triple="true" :operation="true"></button-base>
+    <button-base :label="'/'" :operation="true"></button-base>
+    <button-base :label="'7'"></button-base>
+    <button-base :label="'8'"></button-base>
+    <button-base :label="'9'"></button-base>
+    <button-base :label="'*'"  :operation="true"></button-base>
+  </div>
+</template>
+
+<script>
+import DisplayResult from '@/components/DisplayResult.vue'
+import ButtonBase from '@/components/ButtonBase.vue'
+export default {
+  name: 'CalculatorApp',
+  components: { DisplayResult, ButtonBase },
+}
+</script>
+
+<style>
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr repeat(5, 48px);
+}
+
+</style>
